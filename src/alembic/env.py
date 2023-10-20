67bcde37719f81404f567d6a8a900ba0075b7749
bootBumt <dabajabaza@users.noreply.github.com>
2023-10-20 22:28:36 +0300

    added env variables

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -3,7 +3,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from config import get_db_connection_string
-from chain_health_tgbot.models import *
+from chain_health_tgbot.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/config.py
+++ src/config.py
@@ -18,15 +18,16 @@ def create_sqlite_db_if_it_doesnt_exist(db_url: str) -> None:
 
 def get_db_connection_string() -> str | NotImplementedError:
     config = parse_config()
-    database_section = config.get("database")
-    provider = database_section.get("provider")
-    host = database_section.get("host")
-    port = database_section.get("port")
+    database_section = config.get("database", {})
+    provider = os.getenv("CHAIN_HEALTH_BOT_DB_PROVIDER", database_section.get("provider"))
+    host = os.getenv("CHAIN_HEALTH_BOT_DB_HOST", database_section.get("host"))
+    port = os.getenv("CHAIN_HEALTH_BOT_DB_PORT", database_section.get("port"))
     db_name = os.getenv("CHAIN_HEALTH_BOT_DB_NAME", database_section.get("db_name"))
     username = os.getenv("CHAIN_HEALTH_BOT_DB_USERNAME", database_section.get("username"))
     password = os.getenv("CHAIN_HEALTH_BOT_DB_PASSWORD", database_section.get("password"))
+    data_path = os.getenv("CHAIN_HEALTH_BOT_DB_DATA_PATH", config.get("paths", {}).get("data_path"))
     if provider == "sqlite":
-        filename = os.path.join(config.get("paths").get("data_path"), db_name + ".db")
+        filename = os.path.join(data_path, db_name + ".db")
         db_url = f"{provider}:///{filename}"
         create_sqlite_db_if_it_doesnt_exist(db_url)
         return db_url
